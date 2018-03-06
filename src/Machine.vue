6836e22e385ab17c7f0804b7b4766c8187301b15
Jonathan Courat <jonathan.courat@campus-numerique-in-the-alps.com>
2018-03-06 10:17:45 +0100

    5.1 - creation d'un component et integration de la route

---
+++ src/Machine.vue
@@ -0,0 +1,37 @@
+<template>
+	<div>
+		<h1>Machine {{ name }}</h1>
+		<h3>Status {{ getStatus }}</h3>
+		<h2>Last time checked: {{ checkedAt }}</h2>
+	</div>
+</template>
+
+
+<script>
+	
+	export default {
+		data() {
+			return {
+				name: 'What else ?',
+				status: false,
+				checkedAt: new Date()
+			}
+		},
+		computed: {
+			getStatus() {
+				return this.status ? 'OK' : 'KO';
+			}
+		}
+	}
+
+</script>
+
+<style type="text/css">
+	.red {
+		color: red;
+	}
+
+	.green {
+		color: green;
+	}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,15 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import MachinesList from './MachinesList.vue'
 import MachinesMap from './MachinesMap.vue'
+import Machine from './Machine.vue'
 
 Vue.use(VueRouter);
 
 const routes = [
 	{ path: '/list', component: MachinesList },
-	{ path: '/map', component: MachinesMap}
+	{ path: '/map', component: MachinesMap},
+	{ path: '/machine', component: Machine}
+
 ];
 
 const router = new VueRouter({
