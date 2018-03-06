d6238dea63136e6a37fe3001f0956e6a20efdbe4
Jonathan Courat <jonathan.courat@campus-numerique-in-the-alps.com>
2018-03-06 16:08:35 +0100

    custom the map long lat & css size

--- src/MachinesMap.vue
+++ src/MachinesMap.vue
@@ -2,7 +2,8 @@
 	<div>
 		<h1>Carte des machines</h1>
 		<gmap-map
-		  :center="{lat:10, lng:10}"
+		class="map-google"
+		  :center="{lat:45.1666700, lng:5.7166700}"
 		  :zoom="7"
 		  style="width: 100%; height: 800px"
 		>
@@ -12,4 +13,10 @@
 
 <script></script>
 
-<style type="text/css"></style>
+<style type="text/css">
+	.map-google {
+		margin: auto;
+		max-width:  50vw;
+    	max-height: 50vw;
+	}
+</style>
