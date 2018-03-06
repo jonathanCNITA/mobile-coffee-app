187676ac397e7e486fbd923caaa8a8d2b5052da5
Jonathan Courat <jonathan.courat@campus-numerique-in-the-alps.com>
2018-03-06 11:12:54 +0100

    add Machine in the MachineList component

--- src/MachinesList.vue
+++ src/MachinesList.vue
@@ -1,9 +1,24 @@
 <template>
 	<div>
 		<h1>Liste des machines</h1>
+		<hr/>
+		<app-machine></app-machine>
 	</div>
 </template>
 
-<script></script>
+<script>
+	import Machine from './Machine.vue'
+
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		components: {
+			'app-machine': Machine
+		}
+	}
+</script>
 
 <style type="text/css"></style>
